07c24e6b34df4728009675dad88c3367eaaf1516
Donny-Eiffel <donnyeiffel@gmail.com>
2024-03-23 20:44:22 +0100

    group assignment

---
+++ eiffel.js
@@ -0,0 +1,181 @@
+// variables
+// var number=10;
+// document.getElementById("main").innerHTML=number;
+
+
+
+// let variable
+// function start(){
+//     for (let i=0;i<5;i++ )
+//     console.log(i);
+// }
+// start();
+
+
+
+//const a variable that can't be changed
+// const PI = 3.14159;
+// let radius;
+// let circumference;
+
+// radius = window.prompt("enter the radius of a circle");
+// radius = Number(radius);
+// circumference = 2 * PI * radius;
+// console.log("the circumference is:", circumference);
+
+
+
+// for function
+// function start(){
+//     for (let i=0;i<5;i++ ){
+//         if (true){
+//             var color='red';
+//         }
+//     }
+//     console.log(color);
+// }
+// start();
+
+
+
+// datatypes strings
+// let answer1 = "It's alright";
+// let answer2 = "He is called 'Johnny'";
+// let answer3 = 'He is called "Johnny"';
+// document.getElementById("strings").innerHTML =answer1 + "<br>" + answer2 + "<br>" + answer3;
+
+
+
+
+// datatype number
+// let x1 = 34.00;
+// let x2 = 34;
+// let x3 = 3.14;
+// document.getElementById("demo").innerHTML =x1 + "<br>" + x2 + "<br>" + x3;
+
+
+
+// datatype boolean
+// let x = 5;
+// let y = 5;
+// let z = 6;
+// document.getElementById("demo").innerHTML =(x == y) + "<br>" + (x == z);
+
+
+
+
+// dtatype Array
+// const cars = ["Saab","Volvo","BMW"];
+// document.getElementById("demo").innerHTML = cars[0];
+
+
+
+// datatype Object
+// const person = {
+//     firstName : "John",
+//     lastName  : "Doe",
+//     age     : 50,
+//     eyeColor  : "blue"
+//   };
+// document.getElementById("demo").innerHTML =person.firstName + " is " + person.age + " years old.";
+
+
+
+
+
+// javascript operator
+// document.getElementById("operator").innerHTML = 
+// typeof 0 + "<br>" + 
+// typeof 314 + "<br>" +
+// typeof 3.14 + "<br>" +
+// typeof (3) + "<br>" +
+// typeof (3 + 4);
+
+
+
+
+
+// javascriptassignment
+// Assign the value 5 to x
+// let x = 5;
+// Assign the value 2 to y
+// let y = 2;
+// Assign the value x + y to z
+// let z = x + y;
+// Display z
+// document.getElementById("operator").innerHTML = "The sum of x + y is: " + z;
+
+
+
+// javascript arithmetic operator
+// let a = 3;
+// let x = (100 + 50) * a;
+// document.getElementById("operator").innerHTML = x;
+
+
+
+
+// javascript stringoperator
+// let text1 = "20";
+// let text2 = "5";
+// let result = text1 < text2;
+// document.getElementById("operator").innerHTML = "Is 20 less than 5? " + result;
+
+
+
+
+
+// javascript return Value
+// let x = myFunction(4, 3);
+// document.getElementById("functions").innerHTML = x;
+// function myFunction(a, b) {
+//   return a * b;
+// }
+
+
+// global scope
+// myFunction();
+// code here can use carName as a global variable
+// document.getElementById("demo").innerHTML = "I can display " + carName;
+
+// function myFunction() {
+//   carName = "Volvo";
+// }
+
+// function myFunction() {
+//     var carName = "Volvo";   // Function Scope
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
